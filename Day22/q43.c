dca0aa2217aa618dbd7087e122517101c8db431e
hetkyada <hetkyada0292@gmail.com>
2025-09-13 11:42:08 +0530

    Write a program to check if a number is a strong number.(Loops without Arrays/Strings)

---
+++ Day22/q43.c
@@ -0,0 +1,33 @@
+//Write a program to check if a number is a strong number.(Loops without Arrays/Strings)
+#include <stdio.h>
+
+int factorial(int n) {
+    int fact = 1, i;
+    for (i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num, originalNum, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (num > 0) {
+        digit = num % 10;              
+        sum += factorial(digit);       
+        num /= 10;                     
+    }
+
+    if (sum == originalNum) {
+        printf("%d is a Strong Number.\n", originalNum);
+    } else {
+        printf("%d is NOT a Strong Number.\n", originalNum);
+    }
+
+    return 0;
+}
