3b5b2970aa62783f6e9e592f39b57fe9909d19a5
hetkyada <hetkyada0292@gmail.com>
2025-10-19 13:02:02 +0530

    Perform diagonal traversal of a matrix.(2D Arrays)

---
+++ Day40/q79.c
@@ -0,0 +1,21 @@
+//Perform diagonal traversal of a matrix.(2D Arrays)
+#include<stdio.h>
+int main(){
+int m,n,i,j;
+printf("Enter number of rows and columns: ");
+scanf("%d %d",&m,&n);
+int mat[m][n];
+printf("Enter elements of the matrix:\n");
+for(i=0;i<m;i++)
+for(j=0;j<n;j++)
+scanf("%d",&mat[i][j]);
+printf("Diagonal traversal:\n");
+for(i=0;i<m;i++){
+for(j=0;j<n;j++){
+if(i==j)
+printf("%d ",mat[i][j]);
+}
+}
+printf("\n");
+return 0;
+}
