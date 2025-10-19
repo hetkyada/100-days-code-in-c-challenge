01176d732417fde31702f8c6eb64bc273305a4d7
hetkyada <hetkyada0292@gmail.com>
2025-10-19 17:10:57 +0530

    Write a Program to take an integer array nums. Print an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i]. The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.

--- Day58/q108.c
+++ Day58/q108.c
@@ -1,30 +1,24 @@
-//Write a program to take an array arr[] of integers as input, the task is to find the previous greater element for each element of the array in order of their appearance in the array. Previous greater element of an element in the array is the nearest element on the left which is greater than the current element. If there does not exist next greater of current element, then previous greater element for current element is -1.
-//N.B:
-//- Print the output for each element in a comma separated fashion.
-//- Do not use Stack, use brute force approach (nested loop) to solve.
-
+//Write a Program to take an integer array nums. Print an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i]. The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 #include<stdio.h>
 int main(){
-int n,i,j,found;
+int n,i,j;
 printf("Enter number of elements: ");
 scanf("%d",&n);
-int arr[n],pge[n];
+int nums[n],answer[n];
 printf("Enter elements:\n");
 for(i=0;i<n;i++)
-scanf("%d",&arr[i]);
+scanf("%d",&nums[i]);
 for(i=0;i<n;i++){
-found=0;
-for(j=i-1;j>=0;j--){
-if(arr[j]>arr[i]){
-pge[i]=arr[j];
-found=1;
-break;
-}}
-if(!found)
-pge[i]=-1;
+int prod=1;
+for(j=0;j<n;j++){
+if(i!=j)
+prod*=nums[j];
 }
+answer[i]=prod;
+}
+printf("Output array:\n");
 for(i=0;i<n;i++){
-printf("%d",pge[i]);
+printf("%d",answer[i]);
 if(i<n-1)
 printf(",");
 }
